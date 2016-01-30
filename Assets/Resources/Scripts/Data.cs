92adbc25b429718e485a6c91fda18ba568eaf35c
ladyvipaka <ladyvipaka@yahoo.com>
2016-01-29 21:14:12 -0700

    The data Object in its first iteration
    Nothing to see here yet..

---
+++ Assets/Resources/Scripts/Data.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Data : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		private var teaRoom = new TeaRoom();
+		teaRoom.textArray = ["Wash Hands", "Remove Shoes", "Eat Meal", "Sound Bell"];
+	}
+}
