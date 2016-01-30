6646352da5b1db9620b0bd7df4ca35a8384f2590
Doggyfoofoo <hyyaya@gmail.com>
2016-01-30 14:22:46 -0800

    animations

--- Assets/Resources/Scripts/GameManagerScript.cs
+++ Assets/Resources/Scripts/GameManagerScript.cs
@@ -2,45 +2,40 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class GameManagerScript : MonoBehaviour {
+public class GameManagerScript : MonoBehaviour
+{
 
-	float timeOfDay = 0f; 
-	float gameLength = 1f*10f; 
-	int hours = 0; 
-	int minutes = 0; 
-	string timeString = ""; 
-<<<<<<< HEAD
+	float timeOfDay = 0f;
+	float gameLength = 1f * 10f;
+	int hours = 0;
+	int minutes = 0;
+	string timeString = "";
+
 	float levelTime = 0f;
-=======
 	public float actionTextDecay = 2f;
->>>>>>> origin/master
+
 
 	private ApprovalScript approval;
 	private Text clock;
-	//string[] AsianTeaCeremonySolution = new string[4]; 
+	//string[] AsianTeaCeremonySolution = new string[4];
 
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		approval = GameObject.Find ("ApprovalPrefab").GetComponent<ApprovalScript> ();
 		clock = GameObject.Find ("Clock").GetComponent<Text> ();
-<<<<<<< HEAD
+
 	}
 
-	public float GameLength(){
+	public float GameLength ()
+	{
 		return gameLength; 
-=======
-		GameObject.Find ("ActionText").GetComponent<Text> ().color = Color.clear;
-		/*AsianTeaCeremonySolution[0] = "Mat";
-		AsianTeaCeremonySolution[1] = "Sink";
-		AsianTeaCeremonySolution[2] = "Table";
-		AsianTeaCeremonySolution[3] = "Gong";*/
-
->>>>>>> origin/master
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		UpdateClock ();
 
 		levelTime += Time.deltaTime; 
@@ -59,10 +54,11 @@ public class GameManagerScript : MonoBehaviour {
 	}
 
 
-	void UpdateClock(){
+	void UpdateClock ()
+	{
 		timeOfDay = levelTime / gameLength * 86400f;
-		hours = (int) (timeOfDay / 3600) % 24;
-		minutes = (int) (timeOfDay % 3600)/60;
+		hours = (int)(timeOfDay / 3600) % 24;
+		minutes = (int)(timeOfDay % 3600) / 60;
 
 		if (minutes < 10) {
 			timeString = hours + ":0" + minutes; 
